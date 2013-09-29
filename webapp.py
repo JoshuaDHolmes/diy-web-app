cb13f4ce75169cef87aa75db5234a0a5a3b48990
Joshua Holmes <joshua.holmes@me.com>
2013-09-29 16:56:34 +1000

    Serve Hello World with a template.

--- webapp.py
+++ webapp.py
@@ -1,8 +1,14 @@
-from bottle import route, run
+from bottle import route, run, template, view
 
 @route('/')
 def index():
 	return "Hello World!"
+
+@route('/')
+@route('/greet/<name>')
+@view('hello')
+def greet(name="World"):
+	return {'name': name}
 
 if __name__ == '__main__':
 	run(debug=True, reloader=True, host='0.0.0.0')

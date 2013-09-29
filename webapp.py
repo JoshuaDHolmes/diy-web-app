05202c513f1f4b39af0a70e1fa9ae539df7ffdb6
Joshua Holmes <joshua.holmes@me.com>
2013-09-29 17:56:08 +1000

    Add MongoDB and slogan/noun generator

--- webapp.py
+++ webapp.py
@@ -1,14 +1,38 @@
 from bottle import route, run, template, view, static_file
+from mongoengine import *
 import os
 
 root = os.path.dirname(__file__)
 static_root = os.path.join(root, "static")
 
+connect ('greeter')
+
+class Name(Document):
+	name=StringField(default="World")
+
 @route('/')
-@route('/greet/<name>')
-@view('hello')
-def greet(name="World"):
-	return {'name': name}
+@view('index')
+def slogan():
+	slogan = random.choice(Slogan.objects()).slogan
+	noun = random.choice(Noun.objects()).noun
+	return {'slogan': slogan + ' ' + noun + '.'}
+
+
+@route('/noun/<noun>')
+def saveNoun(noun):
+	# todo: not everyone should be able to set.
+	dbnoun = Noun.objects(noun=noun).first() or Noun()
+	dbnoun.noun = noun
+	dbnoun.save()
+	return template("Saved {{noun}}", {'noun':noun})
+
+@route('/slogan/<slogan>')
+def saveSlogan(slogan):
+	# todo: not everyone should be able to set.
+	dbslogan = Slogan.objects(slogan=slogan).first() or Slogan()
+	dbslogan.slogan = slogan
+	dbslogan.save()
+	return template("Saved {{slogan}}", {'slogan':slogan})
 
 @route('/static/<path:path>')
 def static(path):
